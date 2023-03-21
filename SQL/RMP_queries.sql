68fb0369d938021983a526ecb3ca22d3249ed27b
Greg Salmon <gwsalmon@icloud.com>
2023-03-21 13:19:36 -0700

    Finished uploading data to SQL, cleaned data in python with pandas

--- SQL/RMP_queries.sql
+++ SQL/RMP_queries.sql
@@ -1,3 +1,20 @@
+-- sql file used to view, manipulate and store data to later be used in chrome extension
+-- author: Greg Salmon
+
+
+CREATE TABLE IF NOT EXISTS teachers (
+    teacher_id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    department VARCHAR(255),
+    rating FLOAT,
+    count_ratings INT,
+    difficulty FLOAT,
+    would_retake FLOAT
+);
+
+TRUNCATE TABLE teachers;
+
+
 SELECT COUNT(*)
 FROM teachers;
 
